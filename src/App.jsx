3ebc27110dbe2a0cb7b98ffefcdf30da40c01bc3
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-10-20 10:17:58 +0100

    Using sequence to build an animation timeline.

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,6 @@ import Loader from "./components/Loader"
 function App() {
   return (
    <>
-     <div className='text-red-600'>
-      This is the app
-    </div>
     <Loader />
    </>
   )
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,18 +1,21 @@
 import { useEffect} from "react";
-import { stagger, useAnimate } from "framer-motion";
+import { stagger, useAnimate,motion } from "framer-motion";
 
 function Loader() {
   const words = ['The', 'effect', 'of','art']
   const [scope, animate] = useAnimate();  
+  const sequence = [
+    ['p', {opacity:[0,1]}], 
+    ['p', {x:[0,-100]}, {at: "+0.5"}],
+    ['div',{opacity:[0,1]}, {duration: 0.5}],
+  ]
   useEffect(() => {
-      animate('div', {opacity:[0,1,0]}, {delay:stagger(0.5)})
+      animate(sequence)
   });
   return (
-    <div ref={scope} id="loaderP" className="text-red-500 flex items-center justify-center relative"> 
-      {words.map((word, i) => {
-        i++
-        return <div key={i} className="absolute opacity-0">{word}</div>
-      })}
+    <div ref={scope} className="h-screen w-screen flex items-center justify-center relative">
+      <p className="absolute ">WARNING</p>
+      <div className="absolute ml-5">This is a demo</div>
     </div>
   )
 }
