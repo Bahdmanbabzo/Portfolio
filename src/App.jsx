0336a91a724fe070c8555d383bba206d19648e24
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-10-20 11:37:59 +0100

    Class selectors for sequence are better.

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@ import Loader from "./components/Loader"
 
 function App() {
   return (
-   <>
+   <div className="bg-teal-200">
     <Loader />
-   </>
+   </div>
   )
 }
 
--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,22 +1,24 @@
-import { useEffect} from "react";
+import { useEffect, useRef} from "react";
 import { stagger, useAnimate,motion } from "framer-motion";
 
 function Loader() {
-  const words = ['The', 'effect', 'of','art']
   const [scope, animate] = useAnimate();  
   const sequence = [
-    ['p', {opacity:[0,1]}], 
-    ['p', {x:[0,-100]}, {at: "+0.5"}],
-    ['div',{opacity:[0,1]}, {duration: 0.5}],
+    ['.war', {opacity:[0,1]}], 
+    ['.war', {x:[0,-100]}, {at: "+0.5"}],
+    ['.foo',{opacity:[0,1]}, {duration: 0.5}],
+    ['.clipper',{clipPath:['polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)','polygon(0 0, 0 0, 0 100%, 0% 100%)']},{duration:2}]
   ]
   useEffect(() => {
       animate(sequence)
   });
   return (
-    <div ref={scope} className="h-screen w-screen flex items-center justify-center relative">
-      <p className="absolute ">WARNING</p>
-      <div className="absolute ml-5">This is a demo</div>
-    </div>
+    <div ref={scope} className="h-screen w-screen">
+      <div className="h-full w-full flex items-center justify-center relative bg-yellow-500 clipper">
+        <p className="absolute war">WARNING</p>
+        <div className="absolute ml-5 foo">This is a demo</div>
+      </div>
+  </div>
   )
 }
 
