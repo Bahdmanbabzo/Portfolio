e7430156df2386c0fbe85fc5a79bc8f0ca5aa449
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-10-21 18:56:50 +0100

    Shader for mesh wobble material.

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -1,16 +1,41 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, MeshDistortMaterial, SpotLight } from '@react-three/drei';
 
 const World = () => {
     return (
-        <div className="h-screen w-screen">
+        <div className="h-screen w-screen bg-white">
             <Canvas>
-                <ambientLight intensity={0.5} />
-                <directionalLight position={[0, 10, 5]} intensity={1} />
+                {/* <ambientLight intensity={1} color="white" /> */}
+                <pointLight
+                    position={[0, 2, 1]}
+                    color="red"
+                    intensity={10}
+                    distance={5}
+                />
+                 <pointLight
+                    position={[0, 2, 1]}
+                    color="red"
+                    intensity={10}
+                    distance={5}
+                />
+                 <pointLight
+                    position={[4, 2, 1]}
+                    color="red"
+                    intensity={10}
+                    distance={5}
+                />
+
                 <mesh>
-                    <boxGeometry args={[1, 1, 1]} />
-                    <meshStandardMaterial color="orange" />
+                    <sphereGeometry />
+                    <MeshDistortMaterial 
+                        distort={0.4} 
+                        speed={3}  
+                        color="white"
+                        metalness={0} // Set metalness to 1 for high reflectivity
+                        roughness={1} // Set roughness to 0 for a smooth surface
+                    />
                 </mesh>
+
                 <OrbitControls />
             </Canvas>
         </div>
