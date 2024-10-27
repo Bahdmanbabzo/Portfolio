0cb447874c013696ca7a66f0516a35749516501a
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-10-27 09:51:00 +0100

    Enter portal on click.

--- src/components/Orb.jsx
+++ src/components/Orb.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { useSpring, animated } from '@react-spring/web'
 import { MeshDistortMaterial, Stage, MeshPortalMaterial  } from '@react-three/drei'
 
@@ -6,22 +6,27 @@ const AnimatedMeshDistortMaterial = animated(MeshDistortMaterial)
 
 export default function Orb() {
   const [clicked, setClicked] = useState(false)
+  const ref = useRef(); 
 
   const springs = useSpring({
     color: clicked ? '#569AFF' : '#ff6d6d',
   })
 
-  const handleClick = () => setClicked(s => !s)
+  const handleClick = () => {
+    setClicked(!clicked);
+    console.log(ref.current);
+    ref.current.blend = 1;
+  }
 
   return (
    
     <mesh scale={0.5} onClick={handleClick} castShadow position={[0,0,0]}>
       <planeGeometry/>
-      <MeshPortalMaterial>
+      <MeshPortalMaterial ref={ref}>
         <Stage adjustCamera intensity={0.5} shadows="contact" environment="city">
           <mesh scale={0.5}>
             <sphereGeometry args={[1, 64, 64]} />
-            <AnimatedMeshDistortMaterial
+            < AnimatedMeshDistortMaterial
               speed={2}
               distort={0.2}
               color={springs.color} />
