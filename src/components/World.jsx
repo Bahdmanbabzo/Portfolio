640150abd11f4a063383bdeecbb759c90a151362
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-10-27 09:51:55 +0100

    Added more props to canvas.

--- src/components/World.jsx
+++ src/components/World.jsx
@@ -4,9 +4,21 @@ import Orb from './Orb';
 
 const World = () => {
     return (
-        <div className="h-screen w-screen bg-white">
-            <Canvas shadows camera={{ position: [0, 0, 1] }}>
-                <ambientLight intensity={100} color="yellow" />
+        <div className="h-screen w-screen">
+            <Canvas 
+            shadows 
+            camera={{ position: [0, 0, 1] }}
+            colorManagement
+            gl={{
+                powerPreference:"high-performance",
+                alpha:false, 
+                antialias:false,
+                stencil:false,
+                depth:false
+            }}
+            >
+                <color attach="background" args={['#050505']} />
+                <fog attach="fog" args={['#050505', 8, 40]} />
                 <Orb />
                 <OrbitControls />
             </Canvas>
