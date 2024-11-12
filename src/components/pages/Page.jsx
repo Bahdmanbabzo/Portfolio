a5c561b64d68a2cb6784193b240743546b64252e
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-11-12 16:40:39 +0100

    Horizontal scroll.

--- src/components/pages/Page.jsx
+++ src/components/pages/Page.jsx
@@ -20,11 +20,12 @@ export default function Page() {
                 <p className="absolute text-5xl font-semibold top-1/3 left-1/3"> TRADITION</p>
                 <p className="absolute text-5xl font-semibold top-2/3 left-2/3"> OF EXCELLENCE</p>
             </motion.section>
-            <section className="text-[#fca311] text-3xl font-semibold relative h-full w-full">
-                <p className="absolute text-5xl font-semibold top-0 left-0">CRAFTED IN THE </p>
-                <p className="absolute text-5xl font-semibold top-1/3 left-1/3"> TRADITION</p>
-                <p className="absolute text-5xl font-semibold top-2/3 left-2/3"> OF EXCELLENCE</p>
-            </section>
+            <motion.section 
+                className="text-[#fca311] text-3xl font-semibold relative h-screen w-screen flex flex-none items-center"
+              
+            >
+               <img src="/Lapland,-Finland.jpg" alt="" className="h-1/2 w-1/3 rounded-lg m-6" /><img src="Las-Medulas,-Spain.jpg" alt="" className="h-1/2 w-1/3 rounded-lg m-6" /><img src="Lofoten-Islands- Norway.jpg" alt="" className="h-1/2 w-1/3 rounded-lg m-6" /><img src="Swiss Alps- Switzerland.jpg" alt="" className="h-1/2 w-1/3 rounded-lg m-6" /><img src="Vatnajokull-Iceland.jpg" alt="" className="h-1/2 w-1/3 rounded-lg m-6" />
+            </motion.section>
         </div>
     );
 }
