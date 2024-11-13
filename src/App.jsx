2ee4f8ce06851e79fed641df7a8ccb53de4cbc2b
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-11-13 19:58:26 +0100

    Update App.jsx
    Setting h limit to screen was not responsive with horizontal slide.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import World from "./components/World"
 import Page from "./components/pages/Page"
 function App() {
   return (
-   <div className="bg-teal-200 relative h-screen">
+   <div className="bg-teal-200 relative ">
     {/* <Loader /> */}
     <Page />
     <World />
