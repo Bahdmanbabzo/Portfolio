0935a9e36162be61e3f1eb80d734693a0d49bc5b
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-11-14 21:18:43 +0100

    Reveal card info on hover

--- src/components/pages/Cards.jsx
+++ src/components/pages/Cards.jsx
@@ -1,9 +1,11 @@
+import { motion } from "framer-motion";
+
 export default function Card ({ card }) {
     return (
       <div
         key={card.id}
-        className="group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200"
-      >
+        className="group relative h-[450px] w-[450px] overflow-hidden bg-neutral-200 rounded-xl"
+      > 
         <div
           style={{
             backgroundImage: `url(${card.url})`,
@@ -12,11 +14,12 @@ export default function Card ({ card }) {
           }}
           className="absolute inset-0 z-0 transition-transform duration-300 group-hover:scale-110"
         ></div>
-        <div className="absolute inset-0 z-10 grid place-content-center">
-          <p className="bg-gradient-to-br from-white/20 to-white/0 p-8 text-6xl font-black uppercase text-white backdrop-blur-lg">
-            {card.title}
-          </p>
-        </div>
+        <motion.div 
+          initial={{clipPath: "circle(9.1% at 87% 14%)"}}
+          whileHover={{clipPath:"circle(70.6% at 50% 51%)",backdropFilter: "blur(10px)",  transition:{duration:0.5}}}
+          id='card-overlay'
+          className="absolute z-30 text-white h-full w-full bg-white/30"
+        > lorem 50</motion.div>
       </div>
     );
   };
