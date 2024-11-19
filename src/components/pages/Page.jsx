3d1955a308443cb3ca9a7695ead68a0507c5ac7f
Oserebameh Augustine, Beckley <beckaustine@outlook.com>
2024-11-19 19:45:20 +0100

    Initial structure to animate from.

--- src/components/pages/Page.jsx
+++ src/components/pages/Page.jsx
@@ -1,14 +1,25 @@
-import { motion, useScroll, useTransform } from "framer-motion";
-import { useRef } from "react";
+import { 
+  motion, 
+  useScroll, 
+  useTransform, 
+  useInView, 
+  useAnimate, 
+  stagger 
+} from "framer-motion";
+import { useEffect, useRef } from "react";
 import HorizontalScroll from "./HorizontalScroll";
 
 export default function Page() {
     const { scrollY } = useScroll();
     const ref = useRef();
+    const [scope, animate] = useAnimate(); 
+    const isInView = useInView(scope, {amount:0.3})
 
     // Calculate the opacity based on the scroll position
     // If ref is not mounted, use a default height of 150
     const opacity = useTransform(scrollY, [0, ref.current?.offsetHeight / 2 || 150], [1, 0]);
+
+   
 
     return (
         <div className="relative z-40 px-6">
@@ -18,10 +29,16 @@ export default function Page() {
             <p className="absolute bottom-2 right-4 text-white font-light text-6xl"> SIT AMET</p>  
           </motion.div>
           <HorizontalScroll />
-          <div className="flex h-48 items-center justify-center">
-            <span className="font-semibold uppercase text-neutral-500 h-screen w-screen">
-              Scroll up
-            </span>
+          <div className=" text-white font-light text-6xl h-screen" ref={scope}>
+            <div className="overflow-hidden">
+              <motion.p initial={{y:"100%"}}>Lorem ipsum dolor</motion.p>
+            </div>
+            <div className=" overflow-hidden">
+              <motion.p initial={{y:"100%"}}>Lorem i dont understand</motion.p>
+            </div>
+            <div className=" overflow-hidden">
+              <motion.p initial={{y:"100%"}}>lorem is the name</motion.p>
+            </div>
           </div>
        </div>
     );
